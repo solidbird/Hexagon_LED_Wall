3a63831ca9150529f6a0260bc700d473b61ffe7d
Serdar Akcay <serdar.akcay@alumni.fh-aachen.de>
2025-01-08 02:36:34 +0100

    Partial implementation of packaging the frames from scene by going through topology and rules

--- controller.c
+++ controller.c
@@ -2,48 +2,97 @@
 #include "hexagon.h"
 #include <time.h>
 
-Frame* generate_frames(int frames_amount){
-	Frame* frames = (Frame*) malloc(sizeof(Frame) * frames_amount);
+
+int offset_topology(int x, int y) {
+	return (MIDDLE_OFFSET_INDEX + y) * MIDDLE_OFFSET_INDEX * 2 + (MIDDLE_OFFSET_INDEX + x);
+}
+
+void generate_frames(Scene *scene, int frames_amount){
 
 	for(size_t x = 0; x < frames_amount; x++){
 		for(size_t i = 0; i < 127; i++){
-			frames[x].rgb_value[i] = (RGB_Value){rand() % 255, rand() % 255, rand() % 255};
+			scene[x].rgb_value[i] = (RGB_Value){rand() % 255, rand() % 255, rand() % 255};
 		}
 
 		if(x < (int)(frames_amount/6)*1){
-			frames[x].route = ((uint64_t)3) << 62;
+			/*frames[x].route = ((uint64_t)3) << 62;
 			frames[x].route >>= 2;
 			frames[x].route |= ((uint64_t)2) << 62;
 			frames[x].route >>= 2;
-			frames[x].route |= ((uint64_t)2) << 62;
+			frames[x].route |= ((uint64_t)2) << 62;*/
+			scene[x].destination = (Vector2){0,2};
 		}else if(x < (int)(frames_amount/6)*2){
-			frames[x].route = ((uint64_t)3) << 62;
+			/*frames[x].route = ((uint64_t)3) << 62;
 			frames[x].route >>= 2;
 			frames[x].route |= ((uint64_t)1) << 62;
 			frames[x].route >>= 2;
-			frames[x].route |= ((uint64_t)2) << 62;
+			frames[x].route |= ((uint64_t)2) << 62;*/
+			scene[x].destination = (Vector2){1,0};
 		}else if(x < (int)(frames_amount/6)*3){
-			frames[x].route = ((uint64_t)3) << 62;
+			/*frames[x].route = ((uint64_t)3) << 62;
 			frames[x].route >>= 2;
-			frames[x].route |= ((uint64_t)1) << 62;
+			frames[x].route |= ((uint64_t)1) << 62;*/
+			scene[x].destination = (Vector2){1,1};
 		}else if(x < (int)(frames_amount/6)*4){
-			frames[x].route = ((uint64_t)3) << 62;
+			/*frames[x].route = ((uint64_t)3) << 62;
 			frames[x].route >>= 2;
-			frames[x].route |= ((uint64_t)1) << 62;
+			frames[x].route |= ((uint64_t)1) << 62;*/
+			scene[x].destination = (Vector2){0,2};
 		}else if(x < (int)(frames_amount/6)*5){
-			frames[x].route = ((uint64_t)3) << 62;
+			/*frames[x].route = ((uint64_t)3) << 62;
 			frames[x].route >>= 2;
-			frames[x].route |= ((uint64_t)2) << 62;
+			frames[x].route |= ((uint64_t)2) << 62;*/
+			scene[x].destination = (Vector2){0,1};
 		}else if(x < (int)(frames_amount)){
-			frames[x].route = ((uint64_t)3) << 62;
+			//frames[x].route = ((uint64_t)3) << 62;
+			scene[x].destination = (Vector2){0,0};
 		}
 	}
-	return frames;
 }
 
-void package_frames(Frame *frames, char *topology, Sceen_frames *sceen_frames){
+void asc_sort_edges_cost(Edge_to_node_cost arr[]) {
+   Edge_to_node_cost temp;
+
+    // Compare and swap arr[0] and arr[1] if needed
+    if (arr[0].node_cost > arr[1].node_cost) {
+        temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+    }
+
+    // Compare and swap arr[1] and arr[2] if needed
+    if (arr[1].node_cost > arr[2].node_cost) {
+        temp = arr[1];
+        arr[1] = arr[2];
+        arr[2] = temp;
+    }
+
+    // Compare and swap arr[0] and arr[1] again
+    if (arr[0].node_cost > arr[1].node_cost) {
+        temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+    }
+}
+
+void calculate_slidding_window_average(Topology_node *node){
+	int sum = 0;
+	for(int i = 0; i < 5; i++){
+		sum += node->slidding_window[i];
+	}
+	//__asm__("int $3");
+	node->node_cost = sum/5;
+}
+
+void update_topology_node(Topology_node *node, int x, int y){
+	node[offset_topology(x,y)].slidding_window[node[offset_topology(x,y)].head] = 1;
+	node[offset_topology(x,y)].head = (node[offset_topology(x,y)].head + 1) % 5;
+	calculate_slidding_window_average(&node[offset_topology(x,y)]);
+}
+
+void package_frames(Frame *frames, int frames_amount, Topology_node *topology, Scene *scene){
 	/*
-		- Take each sceen frame and calculate the poisition of each pixel with the color
+		- Take each scene frame and calculate the poisition of each pixel with the color
 		to be on the hexagon panels (lay piuctute or videos on top of the hexagon panels)
 
 		- Have a bunch of Frame packages ready
@@ -52,7 +101,156 @@ void package_frames(Frame *frames, char *topology, Sceen_frames *sceen_frames){
 
 		- return frames
 	*/
+	
+	// 0. Take costs into account prefer always the node that is that has the lowest costs
+	// 1. Go to Y as fast as possible (if possible)
+	// 2. Then take edge 1 priority (so go to X as fast as possible)	
+	for(int i = 0; i < frames_amount; i++){
+		frames[i].route |= ((uint64_t)3) << 62;
+		int x = 0;
+		int y = 0;
+		for(int i = 0; i < MIDDLE_OFFSET_INDEX*2 * MIDDLE_OFFSET_INDEX*2; i++){
+			if(!topology[i].node_available) continue;
+			topology[i].head = (topology[i].head + 1) % 5;
+			topology[i].slidding_window[topology[i].head] = 0;
+		}
+		if(scene[i].destination.x == 0 && scene[i].destination.y == 0){
+			for(int x = 0; x < 127; x++){
+				frames[i].rgb_value[x] = (RGB_Value){scene[i].rgb_value[x].r, scene[i].rgb_value[x].g, scene[i].rgb_value[x].b};
+			}
+			continue;
+		}
 
+		while(scene[i].destination.x != x || scene[i].destination.y != y){
+
+			int peak_index[] = {offset_topology(x+1,y-1), offset_topology(x+1,y), offset_topology(x,y+1)};
+			calculate_slidding_window_average(&topology[peak_index[0]]);
+			calculate_slidding_window_average(&topology[peak_index[1]]);
+			calculate_slidding_window_average(&topology[peak_index[2]]);
+			Edge_to_node_cost sorted_edges[] = {
+				(Edge_to_node_cost){0,topology[peak_index[0]].node_cost},
+				(Edge_to_node_cost){1,topology[peak_index[1]].node_cost},
+				(Edge_to_node_cost){2,topology[peak_index[2]].node_cost}
+			};
+
+			int possible_edges[] = {0,0,0};
+
+			asc_sort_edges_cost(sorted_edges);
+
+			for(int i = 0; i < 2; i++){
+				if(!topology[peak_index[i]].node_available){
+					possible_edges[i] = -1;
+				}
+			}
+
+			if(sorted_edges[0].node_cost != sorted_edges[1].node_cost && sorted_edges[1].node_cost != sorted_edges[2].node_cost && sorted_edges[0].node_cost != sorted_edges[2].node_cost){
+				int edge_to_take = -1;
+				for(int i = 0; i < 3; i++){
+					if(possible_edges[sorted_edges[i].edge] != -1){
+						edge_to_take = sorted_edges[i].edge;
+						break;
+					}
+				}
+				if(edge_to_take != -1){
+					//TAKE THAT EDGE AND CONTINUE WITH MAIN WHILE LOOP
+					frames[i].route >>= 2;
+					switch(edge_to_take){
+						case 0:
+							x++;
+							y--;
+							frames[i].route |= ((uint64_t)0) << 62;
+						break;
+						case 1:
+							x++;
+							frames[i].route |= ((uint64_t)1) << 62;
+						break;
+						case 2:
+							y++;
+							frames[i].route |= ((uint64_t)2) << 62;
+						break;
+					}
+					continue;
+					update_topology_node(topology, x, y);
+					
+					for(int x = 0; x < 127; x++){
+						frames[i].rgb_value[x] = (RGB_Value){scene[i].rgb_value[x].r, scene[i].rgb_value[x].g, scene[i].rgb_value[x].b};
+					}
+				}
+			}else{
+				if(y > scene[i].destination.y){
+					if(possible_edges[sorted_edges[0].edge] != -1){
+						//TAKE THAT EDGE AND CONTINUE WITH MAIN WHILE LOOP
+						x++;
+						y--;
+						update_topology_node(topology, x, y);
+						frames[i].route >>= 2;
+						frames[i].route |= ((uint64_t)0) << 62;
+						for(int x = 0; x < 127; x++){
+							frames[i].rgb_value[x] = (RGB_Value){scene[i].rgb_value[x].r, scene[i].rgb_value[x].g, scene[i].rgb_value[x].b};
+						}
+						continue;
+					}
+				}else if(y < scene[i].destination.y){
+					if(possible_edges[sorted_edges[2].edge] != -1){
+						//TAKE THAT EDGE AND CONTINUE WITH MAIN WHILE LOOP
+						y++;
+						update_topology_node(topology, x, y);
+						frames[i].route >>= 2;
+						frames[i].route |= ((uint64_t)2) << 62;
+						for(int x = 0; x < 127; x++){
+							frames[i].rgb_value[x] = (RGB_Value){scene[i].rgb_value[x].r, scene[i].rgb_value[x].g, scene[i].rgb_value[x].b};
+						}
+						continue;
+					}
+				}
+
+				if(possible_edges[sorted_edges[1].edge] != -1){
+					//TAKE THAT EDGE AND CONTINUE WITH MAIN WHILE LOOP
+					x++;
+					update_topology_node(topology, x, y);
+					frames[i].route >>= 2;
+					frames[i].route |= ((uint64_t)1) << 62;
+					for(int x = 0; x < 127; x++){
+						frames[i].rgb_value[x] = (RGB_Value){scene[i].rgb_value[x].r, scene[i].rgb_value[x].g, scene[i].rgb_value[x].b};
+					}
+					continue;
+				}
+			}
+
+			//TODO: Check if even a node is at that peak location if not than just dont look at it
+/*			if(topology[peak_edge0_index].node_cost <= topology[peak_edge1_index].node_cost){
+				if(topology[peak_edge0_index].node_cost < topology[peak_edge2_index].node_cost){
+					//Go with edge 0
+				}else{
+					//Go with edge 2
+				}
+			}else{
+				if(topology[peak_edge1_index].node_cost < topology[peak_edge2_index].node_cost){
+					//Go with edge 1
+				}else{
+					//Go with edge 2
+				}
+			}
+			
+			//TODO: We want to only do this for one iteration and check the above rule as well before going on with the next iteration
+			while(y != sceen->destination.y){
+				if(!topology[peak_edge2_index].node_available){
+					while(x != sceen->destination){
+						if(!topology[peak_edge1_index].node_available){
+
+						}
+					}
+				}
+				if(y < sceen->destination.y){
+					y++;
+				}else if(y > sceen->destination.y){ //TODO: Check for node_available at edge 0;
+					x++;
+					y--;
+				}
+			}*/
+
+		}
+	}
 }
 
 int forward_process_frame(HexagonPanel* hp, Frame *frame){
@@ -60,6 +258,7 @@ int forward_process_frame(HexagonPanel* hp, Frame *frame){
 		case 0:
 			if(hp->peer_out[0] != NULL){
 				frame->route <<= 2;
+				//TraceLog(LOG_INFO, "FORWARD 0");
 				return 0;
 			}else{
 				TraceLog(LOG_INFO, "Inferface 0 not found");
@@ -68,6 +267,7 @@ int forward_process_frame(HexagonPanel* hp, Frame *frame){
 		case (((uint64_t)1) << 62):
 			if(hp->peer_out[1] != NULL){
 				frame->route <<= 2;
+				//TraceLog(LOG_INFO, "FORWARD 1");
 				return 1;
 			}else{
 				TraceLog(LOG_INFO, "Interface 1 not found");
@@ -76,6 +276,7 @@ int forward_process_frame(HexagonPanel* hp, Frame *frame){
 		case (((uint64_t)2) << 62):
 			if(hp->peer_out[2] != NULL){
 				frame->route <<= 2;
+				//TraceLog(LOG_INFO, "FORWARD 2");
 				return 2;
 			}else{
 				TraceLog(LOG_INFO, "Interface 2 not found");
@@ -83,116 +284,59 @@ int forward_process_frame(HexagonPanel* hp, Frame *frame){
 		break;
 		case (((uint64_t)3) << 62):
 			//Process the package payload and display the content on the matrix
+			//TraceLog(LOG_INFO, "PROCESS");
 			process_rgb_values(hp, frame);
 			return 3;
 		break;
 	}
 }
 
-void master_build_topology(){
-	Discovery_package dp[8];
-	char **topology = NULL;
-	const int offset_index = 128; 
-
-	for(int i = 0; i < 8; i++){
-		dp[i].route_edges = 0;
-	}
-	topology = malloc(sizeof(char*) * (offset_index * 2));
-	for(size_t i = 0; i < offset_index * 2; i++){
-		topology[i] = malloc(sizeof(char) * (offset_index * 2));
-	}
-
-	for(int y = 0; y < offset_index * 2; y++){
-		for(int x = 0; x < offset_index * 2; x++){
-			topology[y][x] = '_';
-		}
-	}
-
-	dp[0].route_edges |= (((uint64_t)1) << 62);
-	dp[0].route_edges |= (((uint64_t)3) << 60);
-
-	dp[1].route_edges |= (((uint64_t)2) << 62);
-	dp[1].route_edges |= (((uint64_t)1) << 60);
-	dp[1].route_edges |= (((uint64_t)3) << 58);
-
-	dp[2].route_edges |= (((uint64_t)2) << 62);
-	dp[2].route_edges |= (((uint64_t)2) << 60);
-	dp[2].route_edges |= (((uint64_t)1) << 58);
-	dp[2].route_edges |= (((uint64_t)3) << 56);
-	
-	dp[3].route_edges |= (((uint64_t)1) << 62);
-	dp[3].route_edges |= (((uint64_t)2) << 60);
-	dp[3].route_edges |= (((uint64_t)1) << 58);
-	dp[3].route_edges |= (((uint64_t)3) << 56);
-	
-	dp[4].route_edges |= (((uint64_t)1) << 62);
-	dp[4].route_edges |= (((uint64_t)2) << 60);
-	dp[4].route_edges |= (((uint64_t)1) << 58);
-	dp[4].route_edges |= (((uint64_t)3) << 56);
-
-	dp[5].route_edges |= (((uint64_t)2) << 62);
-	dp[5].route_edges |= (((uint64_t)1) << 60);
-	dp[5].route_edges |= (((uint64_t)1) << 58);
-	dp[5].route_edges |= (((uint64_t)3) << 56);
-
-	dp[6].route_edges |= (((uint64_t)0) << 62);
-	dp[6].route_edges |= (((uint64_t)2) << 60);
-	dp[6].route_edges |= (((uint64_t)1) << 58);
-	dp[6].route_edges |= (((uint64_t)3) << 56);
-	
-	dp[7].route_edges |= (((uint64_t)1) << 62);
-	dp[7].route_edges |= (((uint64_t)1) << 60);
-	dp[7].route_edges |= (((uint64_t)3) << 58);
-
-	for(size_t i = 0; i < 8; i++){
-		int x = 0;
-		int y = 0;
-
-		while(dp[i].route_edges != (((uint64_t)3) << 62)){
-
-			topology[y + offset_index][x + offset_index] = 'x';
-
-			switch(dp[i].route_edges & (((uint64_t)3) << 62)){
-				// look if topology is already set at case if not then set
-				case 0:
-					y++; x--;
-				break;
-				case (((uint64_t)1) << 62):
-					x++;
-				break;
-				case (((uint64_t)2) << 62):
-					y++;
-				break;
-			}
-			dp[i].route_edges <<= 2;
-		}
-	}
-	// timeout after X sec. if no packages arrived to switch to next state
-}
-
 void* controller_main(void* controller_args){
 	Controller_args *args = (Controller_args *) controller_args;
 	HexagonPanel *hp = args->nodes;
 	HexagonPanel *master = args->master;
+	int frame_size = 300;
+	Scene *scene = (Scene*) malloc(sizeof(Scene) * frame_size);
 
 	Frame *frame = (Frame*) malloc(sizeof(Frame));
-
+	Frame *master_frames = (Frame*) malloc(sizeof(Frame) * frame_size);
+	Topology_node *topology = (Topology_node*)malloc(sizeof(Topology_node) * (MIDDLE_OFFSET_INDEX * 2 * MIDDLE_OFFSET_INDEX * 2));
+	
+	for(int y = 0; y < MIDDLE_OFFSET_INDEX * 2; y++){
+		for(int x = 0; x < MIDDLE_OFFSET_INDEX * 2; x++){
+			int index = offset_topology(x, y);
+			topology[index].node_available = 0;
+			if(x == 0 && y == 0){
+				topology[index].node_cost = -1;
+			}else{
+				topology[index].node_cost = 0;
+			}
+			topology[index].head = 0;
+			for(int i = 0; i < 5; i++){
+				topology[index].slidding_window[i] = 0;
+			}
+		}
+	}
 	//TOxDO: timeout while loop has to run together with all nodes discovery phase
-	char* topology = master_discovery(master, hp, args->nodes_amount);
-	//master_build_topology();
+	master_discovery(master, hp, args->nodes_amount, topology);
 	//TODO: Build actual Frames out of the topology instead of doing this stuff
-	int frame_size = 300;
 	int frame_index = 0;
-	package_frames(master_frames, topology, sceen);
+	generate_frames(scene, frame_size);
+	package_frames(master_frames, frame_size, topology, scene);
 	while(1){
 		//Send frame from Master to all nodes which are connected to
-		//master_discovery(master);
 		master_propegate_frame(master, master_frames, frame_size, &frame_index);
 		node_controller(hp, args->nodes_amount, frame);
+		
+		TraceLog(LOG_INFO, "(0,0): %f", topology[offset_topology(0,0)].node_cost);
+		TraceLog(LOG_INFO, "(0,1): %f", topology[offset_topology(0,1)].node_cost);
+		TraceLog(LOG_INFO, "(0,2): %f", topology[offset_topology(0,2)].node_cost);
+		TraceLog(LOG_INFO, "(1,1): %f", topology[offset_topology(1,1)].node_cost);
+		TraceLog(LOG_INFO, "(1,0): %f", topology[offset_topology(1,0)].node_cost);
 	}
 }
 
-void master_update_topology(HexagonPanel *master, Discovery_package *dp, int middle_offset_index, char *topology){
+void master_update_topology(HexagonPanel *master, Discovery_package *dp, Topology_node *topology){
 	int x = 0;
 	int y = 0;
 	int skip_package = 0;
@@ -214,7 +358,6 @@ void master_update_topology(HexagonPanel *master, Discovery_package *dp, int mid
 			continue;
 		}
 
-
 		switch((dp->route_edges & mask) >> (i*2)) {
 			// look if topology is already set at case if not then set
 			case 0:
@@ -233,8 +376,9 @@ void master_update_topology(HexagonPanel *master, Discovery_package *dp, int mid
 			x = 0;
 			y = 0;
 		}
-		int topology_index = (y + middle_offset_index) * middle_offset_index * 2 + (x + middle_offset_index);
-		topology[topology_index] = 'x';
+
+		int topology_index = (y + MIDDLE_OFFSET_INDEX) * MIDDLE_OFFSET_INDEX * 2 + (x + MIDDLE_OFFSET_INDEX);
+		topology[topology_index].node_available = 1;
 	}
 }
 
@@ -298,7 +442,7 @@ void nodes_discovery(HexagonPanel *nodes, int nodes_amount, Discovery_package *d
 	}
 }
 
-int master_discovery(HexagonPanel *master, HexagonPanel *nodes, int nodes_amount){
+int master_discovery(HexagonPanel *master, HexagonPanel *nodes, int nodes_amount, Topology_node* topology){
 	Discovery_package dp = {.route_edges = 0};
 	Discovery_package *dp_nodes = (Discovery_package*) malloc(sizeof(Discovery_package));
 	dp.route_edges = ~dp.route_edges;
@@ -307,21 +451,14 @@ int master_discovery(HexagonPanel *master, HexagonPanel *nodes, int nodes_amount
 	Discovery_package* topology_info = malloc(sizeof(Discovery_package));
 	DataType type = TYPE_DISCOVERY_PACKAGE;
 
-	int middle_offset_index = 128;
-	char *topology = (char*)malloc(sizeof(char) * (middle_offset_index * 2 * middle_offset_index * 2));
-
-	for(int y = 0; y < middle_offset_index * 2; y++){
-		for(int x = 0; x < middle_offset_index * 2; x++){
-			topology[y * middle_offset_index * 2 + x] = '_';
-		}
-	}
+	
 	int timeout = 300;
 
 	while(timeout){
 		nodes_discovery(nodes, nodes_amount, dp_nodes);
 		if(!ring_buffer_is_empty(master->buffer_out[1])){
 			ring_buffer_pop(master->buffer_out[1], topology_info, &type);
-			master_update_topology(master, topology_info, middle_offset_index, topology);
+			master_update_topology(master, topology_info, topology);
 			timeout = 300;
 		}else{
 			timeout--;
@@ -330,7 +467,7 @@ int master_discovery(HexagonPanel *master, HexagonPanel *nodes, int nodes_amount
 	printf("\n\n---------------\n   TOPOLOGY:   \n---------------\n\n");	
 	for(int y = -5; y < 10; y++){
 		for(int x = -5; x < 10; x++){
-			if(topology[(y + middle_offset_index) * middle_offset_index*2 + (x + middle_offset_index)] == 'x'){
+			if(topology[(y + MIDDLE_OFFSET_INDEX) * MIDDLE_OFFSET_INDEX * 2 + (x + MIDDLE_OFFSET_INDEX)].node_available){
 				printf("(%d,%d)",x ,y);
 			}else{
 				printf("     ");
@@ -345,6 +482,7 @@ void master_propegate_frame(HexagonPanel *master, Frame *master_frames, int fram
 	RingBuffer *send_buffer = master->peer_out[1]->buffer_in[1];
 	ring_buffer_push(send_buffer, (BufferData*) &master_frames[*frame_index], TYPE_FRAME);
 	*frame_index = (*frame_index + 1) % frame_size;
+	
 }
 
 void master_controller(HexagonPanel *master, Master_state *state){
--- controller.h
+++ controller.h
@@ -5,6 +5,8 @@
 #include "hexagon.h"
 #include <pthread.h>
 
+#define MIDDLE_OFFSET_INDEX 64
+
 typedef enum {
 	discovery_phase,
 	run_phase
@@ -16,8 +18,21 @@ typedef struct Controller_args{
 	int nodes_amount;
 } Controller_args;
 
+typedef struct Topology_node{
+	uint8_t node_available;
+	int slidding_window[5];
+	int head;
+	double node_cost;
+} Topology_node;
+
+typedef struct Edge_to_node_cost{
+	uint8_t edge;
+	double node_cost;
+} Edge_to_node_cost;
+
 void* controller_main(void* controller_args);
-Frame* generate_frames(int frames_amount);
+
+void generate_frames(Scene *scene, int frames_amount);
 void master_propegate_frame(HexagonPanel *master, Frame *master_frames, int frame_size, int *frame_index);
 void node_controller(HexagonPanel *node, int nodes_amount, Frame *frame);
 void master_controller(HexagonPanel *master, Master_state *state);
